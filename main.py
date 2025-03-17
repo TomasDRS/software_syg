234e10ece5f65316df7a4f743b30f1fc9f21112b
Tomas SYG <ingenieria@sygcomex.com.ar>
2025-03-17 09:44:58 -0300

    V1.0.10.2 test build

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class UI(QMainWindow):
         uic.loadUi(r"gui\gui.ui", self)  # Cargar la interfaz de Qt Designer
         
         self.user = user
-        self.claseSQLite = SQLite(r"//192.168.10.5/syg/INGENIERIA/PRUEBA_SOFTWARE_MGM/db_test.db")
+        self.claseSQLite = SQLite(r"//192.168.10.5/syg/INGENIERIA/PRUEBA_SOFTWARE_MGM/db.db")
         self.ventana_agregar_evento = ADD_EVENT(user)
         # self.ventana_agregar_empresa = ADD_COMPANY()
 
@@ -28,7 +28,7 @@ class UI(QMainWindow):
         self.msg_login.exec_()
 
         # VERSION DEL PROGRAMA
-        self.action_version.setText("Versión 1.0.10.1 test build")
+        self.action_version.setText("Versión 1.0.10.2 test build")
 
         botones_agregar = [self.boton_agregar_syg_comex, self.boton_agregar_syg_gestion, self.boton_agregar_syg_ingenieria,
                             self.boton_agregar_syg_laboratorio, self.boton_agregar_syg_visitas_ingenieria, self.boton_agregar_syg_producto, 
@@ -318,7 +318,7 @@ class UI(QMainWindow):
             hoy = datetime.today().date()
             fecha_obj = datetime.strptime(ultima_fecha, fmt).date()
 
-            for colindex, value in enumerate([row[0], row[4], row[1], row[2], encargados_formateado, fecha_limite_formateada, finalizado_interno, finalizado]):
+            for colindex, value in enumerate([row[0], row[4], row[1], row[7], row[2], encargados_formateado, fecha_limite_formateada, finalizado_interno, finalizado]):
                 try:
                     tabla_widget.setItem(tableindex, colindex, QtWidgets.QTableWidgetItem(str(value)))
                     tabla_widget.item(tableindex, colindex).setTextAlignment(Qt.AlignCenter)
